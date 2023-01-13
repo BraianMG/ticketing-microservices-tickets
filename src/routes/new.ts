cdbf12782f644b4f533fd2b8d822a9706c20a1b6
Braian Gonzales <braian.gonzales@wakapi.com>
2023-01-13 16:13:51 -0300

    feat(tickets): minor change

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -12,7 +12,8 @@ router.post(
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
   ],
-  validateRequest, async (req: Request, res: Response) => {
+  validateRequest,
+  async (req: Request, res: Response) => {
     const { title, price } = req.body;
 
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
