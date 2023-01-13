508f78879616da2fc2d957262fd33b7244bd11c7
Braian Gonzales <braian.gonzales@wakapi.com>
2023-01-13 11:01:54 -0300

    feat(tickets): test-first approach

---
+++ src/routes/__test__/newTest.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route listening to POST /api/tickets', async () => {});
+
+it('can only be accessed ifthe user is signed in', async () => {});
+
+it('returns an error if an invalid title is provided', async () => {});
+
+it('returns an error f an invalid price is provided', async () => {});
+
+it('creates a ticket with valid inputs', async () => {});
