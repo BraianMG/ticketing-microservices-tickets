f02aa15313faca0c6d76f4b9eadb914c7ef73ff4
Braian <braian.gonzales77@gmail.com>
2024-05-05 19:21:06 +0200

    configure NATS client and NATS wrapper and publish ticket creation event

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import { app } from "./app";
+import { natsWrapper } from '../nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
@@ -8,6 +9,7 @@ const start = async () => {
   if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
 
   try {
+    await natsWrapper.connect('ticketing', 'adasd', 'http://nats-srv:4222'); // here it is "ticketing" because that is how we define it in the arguments of the NATS deployment file in the infra submodule
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
